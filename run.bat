037e36ca71901a04831b87efa1d70c03c1edbe4b
UdayKumarN <95629930+UdayKumarN@users.noreply.github.com>
2022-05-16 08:26:30 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-pabot --processes 2 --outputdir Results  libs/sql.robot
+robot -A .\resources\test.args -d  .\Results\ .\Tests/sql.robot
+#pabot --processes 2 --argumentfile \resources\test.args --outputdir Results  libs/sql.robot
